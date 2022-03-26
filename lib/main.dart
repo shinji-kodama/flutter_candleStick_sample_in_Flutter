3226b48618ba0631790c2b8a426a1491d329d59d
kan11kan <shiraishi-k@clothesoperation.co.jp>
2022-03-26 18:44:43 +0900

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:candlesticks/candlesticks.dart';
 import 'dart:math';
+
+import 'package:candlesticks/candlesticks.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,15 +34,39 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Candle> candles = List.generate(14, (i) {
-    return Candle(date: DateTime(2022, 1, i + 1), open: (i + 1) * 100, high: i * 100 + 150, low: i * 100 - 100, close: i * 100, volume: Random().nextDouble() * 1000);
+    return Candle(
+        date: DateTime(2022, 1, i + 1),
+        open: (i + 1) * 100,
+        high: i * 100 + 150,
+        low: i * 100 - 100,
+        close: i * 100,
+        volume: Random().nextDouble() * 1000);
   });
 
   @override
   void initState() {
     List<Candle> data = [
-      Candle(date: DateTime(2022, 1, 17), open: 300.00, high: 1973.00, low: 100.00, close: 1800.00, volume: 3),
-      Candle(date: DateTime(2022, 1, 16), open: 1800.00, high: 1950.00, low: 300.00, close: 300.00, volume: 2),
-      Candle(date: DateTime(2022, 1, 15), open: 1850.00, high: 1900.00, low: 1750.00, close: 1300.00, volume: 1),
+      Candle(
+          date: DateTime(2022, 1, 17),
+          open: 300.00,
+          high: 1973.00,
+          low: 100.00,
+          close: 1800.00,
+          volume: 3),
+      Candle(
+          date: DateTime(2022, 1, 16),
+          open: 1800.00,
+          high: 1950.00,
+          low: 300.00,
+          close: 300.00,
+          volume: 2),
+      Candle(
+          date: DateTime(2022, 1, 15),
+          open: 1850.00,
+          high: 1900.00,
+          low: 1750.00,
+          close: 1300.00,
+          volume: 1),
     ];
 
     for (var candle in data) {
@@ -57,32 +82,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('demo'),
+        title: const Text('test'),
       ),
       body: SingleChildScrollView(
-        child: 
-          Column(
-            children: [
-              SizedBox(
-                height:200,
-                child: Candlesticks(candles: candles)
-              ),
-              SizedBox(
-                height:200,
-                child: Candlesticks(candles: candles)
-              ),
-
-              SizedBox(
-                height:200,
-                child: Candlesticks(candles: candles)
-              ),
-              SizedBox(
-                height:200,
-                child: Candlesticks(candles: candles)
-              ),
-              
-            ]
-          ),
+        child: Column(children: [
+          SizedBox(height: 200, child: Candlesticks(candles: candles)),
+          SizedBox(height: 200, child: Candlesticks(candles: candles)),
+          SizedBox(height: 200, child: Candlesticks(candles: candles)),
+          SizedBox(height: 200, child: Candlesticks(candles: candles)),
+        ]),
       ),
     );
   }
